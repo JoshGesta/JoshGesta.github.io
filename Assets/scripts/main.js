c97986642502c18dfa18fbe06b88a535f7acca95
JoshGesta <jygesta2003@gmail.com>
2024-08-02 20:22:17 +0800

    first commit

---
+++ Assets/scripts/main.js
@@ -0,0 +1,18 @@
+console.log (myFuncAdd(1,2))
+
+console.log ("Hello World")
+console.log ("Welcome to Dev 101")
+
+
+function myFuncAdd(x,y){
+    return x+y
+}
+function myFuncSub(x,y){
+    return x-y
+}
+function myFuncMul(x,y){
+    return x*y
+}
+function myFuncDiv(x,y){
+    return x/y
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# JoshGesta.github.io
